bb0a94cb780424e25fb7b7789590bb63a94b591a
Hyperlynx <hjake123@gmail.com>
2022-03-06 12:12:51 -0600

    Implement basic profile stuff (no testing yet)

--- src/backend/services/profile_hand.js
+++ src/backend/services/profile_hand.js
@@ -1,3 +1,38 @@
 class Profile{
-    
+    constructor(id, name, addr1, addr2, city, state, zip){
+        this.id = id
+        this.update(name, addr1, addr2, city, state, zip)
+    }
+
+    update(name, addr1, addr2, city, state, zip){
+        this.name = name
+        this.address1 = addr1
+        this.address2= addr2
+        this.city = city
+        this.state = state
+        this.zip = zip
+    }
+}
+let profiles = [] // Temporary until we get a database
+
+function createProfile(id, name, addr1, addr2, city, state, zip){
+    p = Profile(id, name, addr1, addr2, city, state, zip);
+    profiles.push(p)
+}
+
+// Linear search for now, could be faster.
+function getProfile(id){ 
+    for(p in profiles){ 
+        if(p.id == id){
+            return p;
+        }
+    }
+}
+
+function updateProfile(id, name, addr1, addr2, city, state, zip){
+    for(p in profiles){ 
+        if(p.id == id){
+            p.update(name, addr1, addr2, city, state, zip)
+        }
+    }
 }
