4465a8c92d2aabba7efd6cf9fdfc806a1243a8ec
Adil Iqbal <aiqbal85@gmail.com>
2022-03-06 23:42:24 -0600

    redirect!

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -4,15 +4,15 @@ import SampleService from '../services/sample.js';
 const router = Router();
 
 router.post('/account', (req, res) => {
-    res.status(201).redirect('/proto-profile');
+    res.status(308).redirect('/proto-profile');
 });
 
 router.post('/profile', (req, res) => {
-    res.status(201).redirect('/quote');
+    res.status(308).redirect('/quote');
 })
 
 router.get('/logout', (req, res) => {
-    res.status(201).redirect('/login');
+    res.status(308).redirect('/login');
 })
 
 // route for demonstrating unit tests.
