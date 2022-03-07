c3dc63396fb7f5e2237d65e1a4f14125754f2b1d
Adil Iqbal <aiqbal85@gmail.com>
2022-03-06 22:56:41 -0600

    Update server listening message.

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,5 +3,5 @@ import app from './app.js'
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`team-12 assignment-2 listening on port ${port}`)
+  console.log(`Server listening. Try: http://localhost:${port}`)
 });
